ad3543e781090114d773f4ab07f77adaca651dfb
Roberto Guanciale <guancio@gmail.com>
2018-10-02 17:26:11 +0200

    Char driver able to open/close/read

--- minix/drivers/examples/mydriver/mydriver.c
+++ minix/drivers/examples/mydriver/mydriver.c
@@ -76,10 +76,62 @@ static void sef_local_startup()
 }
 
 
+/*
+ * Function prototypes for the hello driver.
+ */
+static int mydriver_open(devminor_t minor, int access, endpoint_t user_endpt);
+static int mydriver_close(devminor_t minor);
+static ssize_t mydriver_read(devminor_t minor, u64_t position, endpoint_t endpt,
+    cp_grant_id_t grant, size_t size, int flags, cdev_id_t id);
+
 /* Entry points to the hello driver. */
 static struct chardriver mydriver_tab =
 {
+ .cdr_open	= mydriver_open,
+ .cdr_close	= mydriver_close,
+ .cdr_read	= mydriver_read,
 };
+
+static int mydriver_open(devminor_t UNUSED(minor), int UNUSED(access),
+                      endpoint_t UNUSED(user_endpt))
+{
+  printf("mydriver_open(). Called %d time(s).\n", ++open_counter);
+  return OK;
+}
+ 
+static int mydriver_close(devminor_t UNUSED(minor))
+{
+  printf("mydriver_close()\n");
+  return OK;
+}
+ 
+static ssize_t mydriver_read(devminor_t UNUSED(minor), u64_t position,
+                          endpoint_t endpt, cp_grant_id_t grant, size_t size, int UNUSED(flags),
+                          cdev_id_t UNUSED(id))
+{
+  u64_t dev_size;
+  char *ptr;
+  int ret;
+  char *buf = HELLO_MESSAGE;
+ 
+  printf("mydriver_read()\n");
+ 
+  /* This is the total size of our device. */
+  dev_size = (u64_t) strlen(buf);
+ 
+  /* Check for EOF, and possibly limit the read size. */
+  if (position >= dev_size) return 0;		/* EOF */
+  if (position + size > dev_size)
+    size = (size_t)(dev_size - position);	/* limit size */
+ 
+  /* Copy the requested part to the caller. */
+  ptr = buf + (size_t)position;
+  if ((ret = sys_safecopyto(endpt, grant, 0, (vir_bytes) ptr, size)) != OK)
+    return ret;
+ 
+  /* Return the number of bytes read. */
+  return size;
+}
 
 int main(void)
 {
