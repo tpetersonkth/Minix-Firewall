23518a933353f83d54605a00d80a072747e401a7
Roberto Guanciale <guancio@gmail.com>
2018-10-02 16:59:13 +0200

    Minimal driver with SEF

--- minix/drivers/examples/mydriver/mydriver.c
+++ minix/drivers/examples/mydriver/mydriver.c
@@ -1,12 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <minix/syslib.h>
+#include <minix/chardriver.h>
 #include "mydriver.h"
  
-int main(int argc, char **argv)
-{
-  sef_startup();          // Explained later
+/* SEF functions and variables. */
+static void sef_local_startup(void);
+static int sef_cb_init(int type, sef_init_info_t *info);
+static int sef_cb_lu_state_save(int, int);
+static int lu_state_restore(void);
+
  
-  printf(HELLO_MESSAGE);
-  return EXIT_SUCCESS;
+/** State variable to count the number of times the device has been opened.
+ * Note that this is not the regular type of open counter: it never decreases.
+ */
+static int open_counter;
+ 
+static int sef_cb_lu_state_save(int UNUSED(state), int UNUSED(flags)) {
+  return OK;
+}
+ 
+static int lu_state_restore() {
+  return OK;
+}
+  
+static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
+{
+  /* Initialize the hello driver. */
+  int do_announce_driver = TRUE;
+ 
+  open_counter = 0;
+  switch(type) {
+  case SEF_INIT_FRESH:
+    printf("%s", HELLO_MESSAGE);
+    break;
+ 
+  case SEF_INIT_LU:
+    /* Restore the state. */
+    lu_state_restore();
+    do_announce_driver = FALSE;
+ 
+    printf("%sHey, I'm a new version!\n", HELLO_MESSAGE);
+    break;
+ 
+  case SEF_INIT_RESTART:
+    printf("%sHey, I've just been restarted!\n", HELLO_MESSAGE);
+    break;
+  }
+
+  /* Announce we are up when necessary. */
+  if (do_announce_driver) {
+    chardriver_announce();
+  }
+ 
+  /* Initialization completed successfully. */
+  return OK;
+}
+
+static void sef_local_startup()
+{
+  /*
+   * Register init callbacks. Use the same function for all event types
+   */
+  sef_setcb_init_fresh(sef_cb_init);
+  sef_setcb_init_lu(sef_cb_init);
+  sef_setcb_init_restart(sef_cb_init);
+ 
+  /*
+   * Register live update callbacks.
+   */
+  sef_setcb_lu_state_save(sef_cb_lu_state_save);
+ 
+  /* Let SEF perform startup. */
+  sef_startup();
+}
+
+
+/* Entry points to the hello driver. */
+static struct chardriver mydriver_tab =
+{
+};
+
+int main(void)
+{
+  /*
+   * Perform initialization.
+   */
+  sef_local_startup();
+ 
+  /*
+   * Run the main loop.
+   */
+  chardriver_task(&mydriver_tab);
+  return OK;
 }
