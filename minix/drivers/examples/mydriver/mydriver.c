c091b9b052d500d8fa5f41dd6ac6a87ec207ca06
Roberto Guanciale <guancio@gmail.com>
2018-10-15 12:57:36 +0200

    INvocation of myservice from mydriver

--- minix/drivers/examples/mydriver/mydriver.c
+++ minix/drivers/examples/mydriver/mydriver.c
@@ -8,6 +8,7 @@
 #include <include/arch/i386/include/archtypes.h>
 #include "kernel/proc.h"
 #include <minix/sysinfo.h>
+#include <minix/myserver.h>
 #include "servers/pm/mproc.h"
 
 /* SEF functions and variables. */
@@ -124,6 +125,8 @@ static int mydriver_open(devminor_t UNUSED(minor), int UNUSED(access),
   received_msg[0] = '\0';
   
   printf("mydriver_open(). Called %d time(s).\n", ++open_counter);
+
+  myserver_sys1();
   return OK;
 }
  
