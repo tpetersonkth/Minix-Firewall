084dadd91e6c160c9865daca07d1fc747f4bd4d7
Pythonpsycho1337 <thomas_pierre_elis@hotmail.com>
2018-11-04 19:14:59 +0100

    Added the functionallity to storing a number and retreiving the corresponding name

--- minix/drivers/group-names/group-names.c
+++ minix/drivers/group-names/group-names.c
@@ -17,12 +17,13 @@ static int sef_cb_init(int type, sef_init_info_t *info);
 static int sef_cb_lu_state_save(int, int);
 static int lu_state_restore(void);
 
-
-/** State variable to count the number of times the device has been opened.
- * Note that this is not the regular type of open counter: it never decreases.
- */
+/* State variable to count the number of times the device has been opened. */
 static int open_counter;
- 
+const int BUF_LEN = 20;
+char received_number[BUF_LEN] = NONUMBERMESSAGE;
+char names[4][20] = {MEMBER1,MEMBER2,MEMBER3,MEMBER4};
+int stored_num = -1;//-1 => Nothing stored
+
 static int sef_cb_lu_state_save(int UNUSED(state), int UNUSED(flags)) {
   return OK;
 }
@@ -39,7 +40,7 @@ static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
   open_counter = 0;
   switch(type) {
   case SEF_INIT_FRESH:
-    printf("%s", HELLO_MESSAGE);
+    printf("Initializing");
     break;
  
   case SEF_INIT_LU:
@@ -102,29 +103,9 @@ static struct chardriver groupNames_tab =
  .cdr_write = groupNames_write,
 };
 
-
-static int vm_debug(endpoint_t ep)
-{
-  message m;
-  int result;
-
-  memset(&m, 0, sizeof(m));
-
-  m.VMPCTL_WHO = ep;
-  result = _taskcall(VM_PROC_NR, VM_PT_DEBUG, &m);
-
-  return(result);
-}
-
-//char recieved_number[20];
-char received_msg[1024];
-int received_pos = 0;
 static int groupNames_open(devminor_t UNUSED(minor), int UNUSED(access),
                       endpoint_t UNUSED(user_endpt))
 {
-  received_pos = 0;
-  received_msg[0] = '\0';
-  
   printf("groupNames_open(). Called %d time(s).\n", ++open_counter);
 
   myserver_sys1();
@@ -144,8 +125,12 @@ static ssize_t groupNames_read(devminor_t UNUSED(minor), u64_t position,
   u64_t dev_size;
   char *ptr;
   int ret;
-  char *buf = MEMBER1;
- 
+
+  char *buf = received_number;
+  if (1 <= stored_num && stored_num <= 4){
+    buf = names[stored_num-1];
+  }
+
   printf("groupNames_read()\n");
  
   /* This is the total size of our device. */
@@ -165,66 +150,37 @@ static ssize_t groupNames_read(devminor_t UNUSED(minor), u64_t position,
   return size;
 }
 
-struct proc proc[NR_TASKS + NR_PROCS];
-struct mproc mproc[NR_PROCS];
 static ssize_t groupNames_write(devminor_t UNUSED(minor), u64_t position,
 			  endpoint_t endpt, cp_grant_id_t grant, size_t size, int UNUSED(flags),
 			  cdev_id_t UNUSED(id))
 {
   int r;
   printf("groupNames_write(position=%llu, size=%zu)\n", position, size);
-  
-  if (size > 1023 - received_pos)
-    size = (size_t)(1023 - received_pos);	/* limit size */
 
-  //r = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) (received_number), size);
-  r = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) (received_msg+received_pos), size);
+  if (size > BUF_LEN)//Avoid writing outside of buffer..
+    size = (size_t)(BUF_LEN);
+
+  r = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) (received_number), size);
   if (r != OK) {
     printf("GROUPNAMES: warning: couldn't copy data %d\n", r);
     return OK;
   }
-  received_pos += size;
-  received_msg[received_pos] = '\0';
-  printf("received=%s\n", received_msg);
+  //received_number[size] = '\0';//Maybe not needed?
+  printf("received=%s\n", received_number);
 
-  if (received_msg[received_pos-1] != '\n')
-    return size;
-  int pid = atoi(received_msg);
+  stored_num = atoi(received_number);
+  printf("Stored number:%d\n", stored_num);
 
-  /* Retrieve and check the PM process table. */
-  r = getsysinfo(PM_PROC_NR, SI_PROC_TAB, mproc, sizeof(mproc));
-  if (r != OK) {
-    printf("GROUPNAMES: warning: couldn't get copy of PM process table: %d\n", r);
-    return OK;
-  }
-  endpoint_t end_p = 0;
-  for (int mslot = 0; mslot < NR_PROCS; mslot++) {
-    if (mproc[mslot].mp_flags & IN_USE) {
-      printf("%d %d\n", mproc[mslot].mp_pid, mproc[mslot].mp_endpoint);
-      if (mproc[mslot].mp_pid == pid)
-        end_p = mproc[mslot].mp_endpoint;
-    }
-  }
-  
-  vm_debug(end_p);
-
-  received_pos = 0;
-  received_msg[0] = '\0';
-  
   return size;
 }
 
 
 int main(void)
 {
-  /*
-   * Perform initialization.
-   */
+  //Perform initialization.
   sef_local_startup();
- 
-  /*
-   * Run the main loop.
-   */
+
+  //Run the main loop.
   chardriver_task(&groupNames_tab);
   return OK;
 }
