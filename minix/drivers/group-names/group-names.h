af6d748ddb16b421f7f72a5218b917f50cf95742
thpeter <thpeter@kth.se>
2018-11-04 19:14:59 +0100

    Added the functionallity to storing a number and retreiving the corresponding name

--- minix/drivers/group-names/group-names.h
+++ minix/drivers/group-names/group-names.h
@@ -1,8 +1,7 @@
 #ifndef __GROUPNAMES_H
 #define __GROUPNAMES_H
- 
-/** The Hello, World! message. */
-#define HELLO_MESSAGE "\n"
+
+#define NONUMBERMESSAGE "NO NUMBER STORED\n"
 #define MEMBER1 "Davis Freimanis\n"
 #define MEMBER2 "Thomas Peterson\n"
 #define MEMBER3 "Emelie Eriksson\n"
