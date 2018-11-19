e1436d33e02e2adf9c00dbb8c8c5616625565b9c
Pythonpsycho1337 <thpeter@kth.se>
2018-11-19 20:59:58 +0100

    Added a print statement to the service and comment

--- minix/servers/decision/decision.c
+++ minix/servers/decision/decision.c
@@ -7,13 +7,14 @@
  *===========================================================================*/
 int sef_cb_init_fresh(int UNUSED(type), sef_init_info_t *info)
 {
+  printf("Decision server started\n");
   return(OK);
 }
 
 /*===========================================================================*
  *				do_publish				     *
  *===========================================================================*/
-int do_sys1(message *m_ptr)
+int do_sys1(message *m_ptr)//This function can not be used yet since I haven't set up messages for this server yet /Thomas
 {
   printf("invoked the syscall 01\n");
   return(OK);
