b67d4cefbaf4aed8ca596130006112f2afa8fafc
Davis Freimanis <davisfreimanis@gmail.com>
2018-12-04 14:49:22 +0100

    Message passing example to the fwdec server

--- minix/lib/libsys/fwdec.c
+++ minix/lib/libsys/fwdec.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "syslib.h"
+#include <stdio.h>
 
 static int do_invoke_fwdec(message *m, int type)
 {
@@ -15,8 +16,14 @@ static int do_invoke_fwdec(message *m, int type)
 
 int fwdec_check_packet(void)
 {
+	printf("libsys/fwdec_check_packet\n");
 	message m;
-
 	memset(&m, 0, sizeof(m));
+
+	/* Prepare the request message for the firewall */
+	m.m_type = FWDEC_CHECK_PACKET;
+	m.m_fw_test.test1 = 1;
+	m.m_fw_test.test2 = 222;
+
 	return do_invoke_fwdec(&m, FWDEC_CHECK_PACKET);
 }
